f3ea3941eca7cc969984d69dcc6d80fd5e2296ab
Wiezien <145956394+Wiezien@users.noreply.github.com>
2024-08-29 18:14:59 +0100

    some words list

--- basic.py
+++ basic.py
@@ -1,2 +1,6 @@
 import random
 from collections import Counter
+
+someWords = '''apple banana mango strawberry 
+orange grape pineapple apricot lemon coconut watermelon 
+cherry papaya berry peach lychee muskmelon'''
